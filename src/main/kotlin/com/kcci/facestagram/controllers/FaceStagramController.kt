5f7f1da1e55888a6f687a2ac04f1eb5dc46cdc2d
hutturo <ghdsuk95@naver.com>
2019-11-25 16:20:10 +0900

    controllers package add

---
+++ src/main/kotlin/com/kcci/facestagram/controllers/FaceStagramController.kt
@@ -0,0 +1,10 @@
+package com.kcci.facestagram.controllers
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping
+class FaceStagramController {
+
+}
